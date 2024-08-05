3194ea59fca90d47304046181f2a5b8b0233127e
varunsatish <varun.satish@princeton.edu>
2024-08-05 15:08:26 -0400

    added to .gitignore

--- recipes/3p_integrations/lamini/text2sql_memory_tuning/util/get_default_finetune_args.py
+++
@@ -1,8 +0,0 @@
-def get_default_finetune_args():
-    return {
-        "learning_rate": 3e-4,
-        "max_steps": 360,
-        "early_stopping": False,
-        "load_best_model_at_end": False,
-        "peft_args": {"r_value": 32},
-    }
